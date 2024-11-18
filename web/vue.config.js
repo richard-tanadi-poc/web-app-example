c90aa9ade2c8958bc15bcf617500198816b37da2
richard-tanadi <richard.tanadi@project.mastersystem.co.id>
2024-11-18 15:25:15 +0700

    poc-17

--- web/vue.config.js
+++ web/vue.config.js
@@ -4,6 +4,11 @@ module.exports = {
     disableHostCheck: true,
     public: '',
     progress: false,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+    },
     proxy: {
       '^/api': {
         target: 'http://192.168.1.242/api',
@@ -11,6 +16,9 @@ module.exports = {
         changeOrigin: true,
         secure: false,
         logLevel: 'debug',
+        pathRewrite: {
+           '^/api': ''
+        }
       },
     },
   },
